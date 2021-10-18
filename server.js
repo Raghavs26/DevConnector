5f87540b0434af14e70405b59d4d8b1c7f0612a0
Raghav <raghavsekhri2k1@gmail.com>
2021-10-18 18:56:14 +0530

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//connecting database
+connectDB();
 
 app.get("/", (req, res) => res.send("API RUNNING"));
 
 const PORT = process.env.PORT || 5000;
+
+//routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
