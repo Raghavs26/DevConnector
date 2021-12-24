729423f8fd4be87dfc6e4d2974c6042e1a4e4c8f
Raghav <raghavsekhri2k1@gmail.com>
2021-12-24 17:55:41 +0530

    protected route & get current profile

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,22 @@
+import { GET_PROFILE, PROFILE_ERROR } from "../actions/types";
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {},
+};
+
+// eslint-disable-next-line
+export default function (state = initialState, action) {
+  const { payload, type } = action;
+  switch (type) {
+    case GET_PROFILE:
+      return { ...state, profile: payload, loading: false };
+    case PROFILE_ERROR:
+      return { ...state, error: payload, loading: false };
+    default:
+      return state;
+  }
+}
